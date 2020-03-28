1421969c1b2550424ddd887c42ac248ac335b245
Matthias Möller <m_moeller@live.de>
2020-03-28 21:22:06 +0100

    adding simple test for the solver

--- solver/src/solver.rs
+++ solver/src/solver.rs
@@ -65,3 +65,43 @@ pub fn solve(grid: &Grid) -> Option<Grid> {
         Some(m) => return Some(Grid::from_model(&m)),
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_solver(){
+        let mut grid = Grid::new();
+        grid.data = [
+            8,0,0,0,0,0,0,0,0,
+            0,0,3,6,0,0,0,0,0,
+            0,7,0,0,9,0,2,0,0,
+            0,5,0,0,0,7,0,0,0,
+            0,0,0,0,4,5,7,0,0,
+            0,0,0,1,0,0,0,3,0,
+            0,0,1,0,0,0,0,6,8,
+            0,0,8,5,0,0,0,1,0,
+            0,9,0,0,0,0,4,0,0
+        ];
+        let mut solution = Grid::new();
+        solution.data = [
+            8,1,2,7,5,3,6,4,9,
+            9,4,3,6,8,2,1,7,5,
+            6,7,5,4,9,1,2,8,3,
+            1,5,4,2,3,7,8,9,6,
+            3,6,9,8,4,5,7,2,1,
+            2,8,7,1,6,9,5,3,4,
+            5,2,1,9,7,4,3,6,8,
+            4,3,8,5,2,6,9,1,7,
+            7,9,6,3,1,8,4,5,2
+        ];
+
+        let computated = solve(&grid).unwrap();
+
+        for i in 0..81 {
+            assert_eq!(computated.data[i], solution.data[i]);
+        }
+
+    }   
+}
